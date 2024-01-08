e4a90d586422059461e7428fa955bdd28a360293
harsh <harshpriyam.official@gmail.com>
2024-01-09 01:00:23 +0530

    Data Ingestion Stage Created and Integrated with DvC

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from CarPrediction import logger 
+from CarPrediction.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logging")
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/CarPrediction/utils/common.py
+++ src/CarPrediction/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from src.CarPrediction import logger
+from CarPrediction import logger
 import json
 import joblib
 from ensure import ensure_annotations
